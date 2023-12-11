f23c86d00c20bc00dbe91969dc90dbeea8b56619
Rainata Putra <kriegcom77@gmail.com>
2023-12-11 08:55:44 +0700

    Free pas w.o login

--- src/private-route/PrivateRoute.js
+++ src/private-route/PrivateRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
     const user = JSON.parse(localStorage.getItem('user'));
-    return user ? children : <Navigate to={'/auth'}/>;
+    return user ? children : children //<Navigate to={'/auth'}/>;
 }
  
 export default PrivateRoute;
