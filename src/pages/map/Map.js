5393f0f1039d80f703be41078acb0325d9bbd63d
Rainata Putra <kriegcom77@gmail.com>
2024-02-05 12:59:40 +0700

    Adding label based on status on MAP

--- src/pages/map/Map.js
+++ src/pages/map/Map.js
@@ -1,7 +1,12 @@
-import GoogleMapReact from 'google-map-react';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { useState, useMemo, useId, useEffect } from 'react';
+import { createPortal } from "react-dom";
+import L from 'leaflet';
 import { useNavigate } from 'react-router-dom';
+import { useStatistic } from '../../hooks/useStatistic';
+import { useGetData } from '../../hooks/useGetData';
 
-const Marker = ({text}) => {
+const MarkerCustom = ({text, color}) => {
     const navigate = useNavigate()
     const handleNavigate = () => {
         if (text.startsWith('AWLR') || text.startsWith('awlr')) {
@@ -16,10 +21,50 @@ const Marker = ({text}) => {
             <div className="w-[30px] h-[30px] rounded-full overflo-hidden shadow border border-blue-700 mr-2">
                 <img src="https://cdn-icons-png.flaticon.com/512/119/119573.png" alt="ICN" className='w-full h-full object-cover'/>
             </div>
-            <p className='rounded-md bg-yellow-100 px-3 text-lg whitespace-nowrap group-hover:bg-yellow-400'>{text}</p>
+            <p className={`${text.startsWith('AWLR') ? 'rounded-full' : ''} ${color} px-3 text-lg whitespace-nowrap`}>{text}</p>
         </div>
     )
 }
+
+const EnhancedMarker = ({
+    eventHandlers,
+    icon: providedIcon,
+    ...otherProps
+  }) => {
+    const [markerRendered, setMarkerRendered] = useState(false);
+    const id = "marker-" + useId();
+  
+    const icon = useMemo(
+      () =>
+        L.divIcon({
+          html: `<div id="${id}"></div>`,
+          className: 'dummy'
+        }),
+      [id]
+    );
+  
+    return (
+      <>
+        <Marker
+          {...otherProps}
+          eventHandlers={{
+            ...eventHandlers,
+            add: (...args) => {
+              setMarkerRendered(true);
+              if (eventHandlers?.add) eventHandlers.add(...args);
+            },
+            remove: (...args) => {
+              setMarkerRendered(false);
+              if (eventHandlers?.remove) eventHandlers.remove(...args);
+            },
+          }}
+          icon={icon}
+        />
+        {markerRendered &&
+          createPortal(providedIcon, document.getElementById(id))}
+      </>
+    );
+  };
 
 const Map = () => {
     const stasiun = {
@@ -34,12 +79,70 @@ const Map = () => {
           lng: 112.760337
         },
         zoom: 11
-      };
-
-    const handleApiLoaded = (map, maps) => {
-    // use map and maps objects
-    // console.log(map); console.log(maps);
     };
+
+    const colorMapStatus = {
+      'Aman' : 'bg-green-400 group-hover:bg-green-500',
+      'Siaga' : 'bg-yellow-400 group-hover:bg-yellow-500',
+      'Bahaya' : 'bg-red-400 group-hover:bg-red-500',
+      "Undefined": 'bg-white'
+    }
+
+    const getStatus = (value, stasiunLimitAir, item) => {
+      if (!stasiunLimitAir) {
+        return "Undefined"
+      }
+      return value <= stasiunLimitAir[0] ? "Aman" : value > stasiunLimitAir[0] && value < stasiunLimitAir[1] ? "Siaga" : value >= stasiunLimitAir[1] ? "Bahaya" : "Undefined"
+    }
+
+    const [loading, setIsLoading] = useState(true);
+    const [aktualData, setAktualData] = useState({});
+    const [limitAir, setLimitAir] = useState({});
+    const {getChartData} = useStatistic();
+    const {getStasiunLimitAir} = useGetData();
+
+    useEffect(() => {
+      const loadData = async () => {
+        const stationNames = Object.keys(stasiun)
+        const data = await Promise.all(
+          stationNames.map(async item => {
+            const [typ, stasiunName] = item.split(" ")
+  
+            // check if awlr or arr
+            if (typ === "AWLR") {
+              const res = await getChartData("def", "lstm", stasiunName, 5)
+              const resLimitAir = await getStasiunLimitAir("def", stasiunName === "Dhompo" ? 1 : 2)
+              const {batas_air_siaga, batas_air_awas} = resLimitAir.data || [-1, -1]
+              let newLimitAir = limitAir
+              newLimitAir[item] = [batas_air_siaga, batas_air_awas]
+              setLimitAir(newLimitAir)
+
+              let aktual = -1;
+              if (res) {
+                res.data.map(item => {
+                  if (item.aktual) {
+                      aktual = item.aktual
+                  }
+                })
+              }
+              // set the actual data
+              let newData = aktualData
+              newData[item] = aktual
+              setAktualData(newData)
+            } else {
+
+            }
+          })
+        )
+
+        setIsLoading(false)
+      }
+
+      // call the functions
+      loadData()
+    }, [])
+
+
     return ( 
         <>
             <div className='text-left'>
@@ -47,26 +150,24 @@ const Map = () => {
                 <p className="text-sm italic mb-5">Sistem Peramalan dan Peringatan Banjir</p>
             </div>
             <p className="text-sm text-black italic mt-5 font-semibold">Petunjuk :  untuk melihat peramalan dan peringatan dini di stasiun monitoring, silakan klik titik stasiun monitoring yang diinginkan</p>
-            <div className="w-full h-[45%]">
-                <GoogleMapReact
-                    bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_API_KEY }}
-                    defaultCenter={defaultProps.center}
-                    defaultZoom={defaultProps.zoom}
-                    yesIWantToUseGoogleMapApiInternals
-                    onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-                >
-                    {Object.keys(stasiun).map((item, i) => (
-                        <Marker text={item} key={i} lat={stasiun[item][0]} lng={stasiun[item][1]}/>
+            <div className="w-full">
+                <MapContainer center={[defaultProps.center.lat, defaultProps.center.lng]} zoom={defaultProps.zoom} scrollWheelZoom={false} style={{height: '50vh'}}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {!loading && Object.keys(stasiun).map((item, i) => (
+                        <EnhancedMarker position={stasiun[item]} key={i} icon={<MarkerCustom text={item} color={colorMapStatus[getStatus(aktualData[item], limitAir[item], item)]}/>}/>
                     ))}
-                </GoogleMapReact>
+                </MapContainer>
             </div>
             <div className="flex flex-row">
-            <div className="w-[40%] h-[40%]">
-                <img src="Hilir.gif" alt="" className="w-full object-contain"/>
-            </div>
-            <div className="w-[40%] h-[40%]">
-                <img src="Hulu.gif" alt="" className="w-full object-contain"/>
-            </div>
+                <div className="w-[40%] h-[40%]">
+                    <img src="Hilir.gif" alt="" className="w-full object-contain"/>
+                </div>
+                <div className="w-[40%] h-[40%]">
+                    <img src="Hulu.gif" alt="" className="w-full object-contain"/>
+                </div>
             </div>
         </>
      );
